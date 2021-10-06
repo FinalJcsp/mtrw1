8220db217fd5fa6681f6fa8e84a8aa10d331af4b
FinalJcsp <87635251+FinalJcsp@users.noreply.github.com>
2021-10-06 17:14:16 +0800

    Add files via upload

--- stack/Stack.java
+++ stack/Stack.java
@@ -1,87 +1,78 @@
 package stack;
 
 /**
- * creat by FinalJ on 2021 9 25.
- *  描述：构建一个泛型堆栈MyStack<T>，要求实现以下功能
- *  入栈（方法名：push）
- *  出栈（方法名：pop）
- *  返回栈顶元素（方法名：top）
- *  要求：
- *  必须是泛型
- *  不能使用Java内部提供的堆栈(Stack)类
- *
- * @author 86186*/
+ * creat by FinalJ on 2021 10 6.
+ **/
+/**
+ * @author 86186
+ */
+
 public class Stack {
     public static void main(String[] args){
-    MyStack<Integer> stack=new MyStack<>(10);
-    stack.push(5);
-    stack.getTop();
-    stack.pop();
+        MyStack<Integer> stack=new MyStack<>();
+        stack.push(5);
+        System.out.println(stack.top());
+        stack.push(6);
+        stack.pop();
+        stack.pop();
+        System.out.println(stack.top());
     }
 }
 
 class MyStack<T>{
-    private int size;
-    private int top=-1;
-    private Object[] stack;
 
-    /**
-    * 为栈初始化容量
-    * */
-    public MyStack(int n){
-        stack=new Object[n];
-        size=n;
+    class Node<T>{
+        private T data;
+        private Node<T> next=null;
     }
 
-    /**
-     * 判断栈是否满了
-    **/
-    public boolean isFull(){
-        return top==size-1;
+    private Node<T> head;
+
+    public MyStack(){
+        head=null;
     }
 
-    /**
-     * 判断栈是否为空
-     **/
-    public boolean isEmpty(){
-        return top==-1;
-    }
-
-    /**
-     * 入栈
-     * */
-    public void push(int x){
-        if(isFull()){
-           System.out.println("栈满了");
+    public void isEmpty() throws Exception{
+        if(head==null){
+            throw new Exception("栈为空");
         }
+    }
+
+    public void push(T t){
+        if(head==null){
+            head=new Node<T>();
+            head.data=t;
+            head.next=null;
+        }
+
         else{
-            stack[++top]=x;
+            Node<T> temp=null;
+            temp=head;
+            head=new Node<T>();
+            head.next=temp;
+            head.data=t;
         }
     }
 
-    /**
-     * 出栈
-     * */
     public void pop(){
-        if(isEmpty()){
-            System.out.println("栈空了");
+        try{
+            isEmpty();
         }
-        else{
-            System.out.println(stack[top]+"  删除成功  ");
-            top--;
+        catch(Exception e){
+            System.out.println(e.getMessage());
         }
+        head=head.next;
+        System.out.println("删除成功");
     }
 
-    /**
-     * 返回栈顶元素
-    **/
-    public void getTop(){
-        if(isEmpty()){
-            System.out.println("栈空了");
+    public T top(){
+        try{
+            isEmpty();
         }
-        else{
-            System.out.println(stack[top]);
+        catch(Exception e){
+            System.out.println(e.getMessage());
         }
+        T t=head.data;
+        return t;
     }
-
 }
